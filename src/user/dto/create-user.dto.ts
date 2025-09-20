20380dff0c702b7103c691b98aa3de9d597e0617
ptakj <ptakjakub004@gmail.com>
2025-09-20 13:43:21 +0200

    create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import {
+  IsOptional,
+  IsString,
+  MaxLength,
+} from "class-validator";
+
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty()
+  email: string;
+
+
+  @IsString()
+  @ApiProperty()
+  @MaxLength(50)
+  password: string;
+
+
+  @IsOptional()
+  @MaxLength(50)
+  @IsString()
+  @ApiPropertyOptional()
+  name?: string;
+
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional()
+  surname?: string;
+}
