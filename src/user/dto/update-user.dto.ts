5afe771b6db98413b52543b889a7479de8b4ff3f
ptakj <ptakjakub004@gmail.com>
2025-09-20 13:46:39 +0200

    update user dto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import {
+  IsOptional,
+  IsString,
+  MaxLength,
+} from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+
+export class UpdateUserDto  {
+   @IsString()
+  @ApiPropertyOptional()
+  @IsOptional()
+  email?: string;
+
+
+  @ApiPropertyOptional()
+  @IsString()
+  @ApiProperty()
+  @MaxLength(50)
+  password?: string;
+
+
+  @IsOptional()
+  @MaxLength(50)
+  @IsString()
+  @ApiPropertyOptional()
+  name?: string;
+
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional()
+  surname?: string;
+}
